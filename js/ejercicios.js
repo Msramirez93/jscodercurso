4c315d1628038e3b58f07fa4d258aab11375206f
Msramirez93 <melany.salome.ramirez@gmail.com>
2021-09-04 16:52:59 -0300

    ejercicios de js

---
+++ js/ejercicios.js
@@ -0,0 +1,70 @@
+//ciclo FOR
+
+//for (let i = 0; i <= 30; i++) {
+    //if(i>6) break
+    
+    //alert(i);
+    //console.log(i);
+//}
+//continue interrumpe el conteo y luego lo retoma (en este caso es los numeros pares)
+/*for (let i = 0; i <= 10; i++) {
+    if(i % 2 == 0) continue;
+
+    console.log(i)
+}*/
+//let numerodeUsuario =30
+
+/*for(let i= 1; i<= numerodeUsuario; i++){
+    if (i == 10){
+        break
+    }
+    console.log('la cantidad de usuarios es' +" "+ i)
+}*/
+/*let ingreseNumero= parseInt(prompt("ingrese un numero"));
+
+for(let i =1; i<=10; i++){
+    let resultado = ingreseNumero *i;
+    console.log(ingreseNumero + "X" + i +" = "+ resultado);
+}*/
+
+/*for(let i = 1; i <= 20; i++){
+     let ingresaNombre = prompt("Ingresar Nombre");
+     alert(" Turno N° "+i+"Nombre: "+" "+ingresaNombre);
+}*/
+
+//let entrada = prompt("Ingresar un dato");
+
+//while(entrada != "ESC"){
+  //  alert("El usuario ingreso "+ entrada);
+    //entrada = prompt("ingresar otro dato");
+//}
+
+/* do while ejercicio*/
+
+//let numero = 0;
+
+//do{
+  //  numero =prompt('ingresar un Numero');
+  //  console.log(numero);
+
+//}while(parseInt(numero));
+
+//while y Switch
+
+//let entrada = prompt("ingresar un nombre");
+
+/*while(entrada != "ESC"){
+    switch (entrada){
+        case "ANA":
+            alert("HOLA ANA");
+            break;
+            case "JUAN":
+                alert("HOLA JUAN");
+                break;
+            default:
+                alert("¿QUIEN SOS?")
+                break;
+    }
+    entrada = prompt("ingresar un nombre");
+}*/
+/* ejercicios de la clase 3*/
